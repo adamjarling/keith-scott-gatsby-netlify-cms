8da22d8a454c2ec41f89c5bb2e8a8466b9ab978f
Adam Arling <aarling@gmail.com>
2019-12-02 18:20:42 -0600

    Pre eliminate custom Keith CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,57 @@
 import { Link } from "gatsby";
 import React, { useState } from "react";
 import signature from "../images/keith-logo-horizontal.png";
+import { globalHistory as history } from "@reach/router";
 
-const Nav = () => (
-  <ul className="menu">
-    <li className="menu-item current-menu-item">
-      <Link to="/">Home</Link>
-    </li>
-    <li className="menu-item">
-      <Link to="/about/">About</Link>
-    </li>
-    <li className="menu-item">
-      <Link to="/gallery/">Gallery</Link>
-    </li>
-    <li className="menu-item">
-      <Link to="/music/">Music</Link>
-    </li>
-    <li className="menu-item">
-      <Link to="/tour-dates/">Tour Dates</Link>
-    </li>
-    <li className="menu-item">
-      <a href="https://goo.gl/NREiMX" target="_blank">
-        Videos
-      </a>
-    </li>
-  </ul>
-);
+const Nav = () => {
+  const { location, navigate } = history;
+  const pathName = location.pathname;
 
-const Header = () => {
+  return (
+    <ul className="menu">
+      <li
+        className={`menu-item ${pathName === "/" ? "current-menu-item" : ""}`}
+      >
+        <Link to="/">Home</Link>
+      </li>
+      <li
+        className={`menu-item ${
+          pathName === "/about/" ? "current-menu-item" : ""
+        }`}
+      >
+        <Link to="/about/">About</Link>
+      </li>
+      <li
+        className={`menu-item ${
+          pathName === "/gallery/" ? "current-menu-item" : ""
+        }`}
+      >
+        <Link to="/gallery/">Gallery</Link>
+      </li>
+      <li
+        className={`menu-item ${
+          pathName === "/music/" ? "current-menu-item" : ""
+        }`}
+      >
+        <Link to="/music/">Music</Link>
+      </li>
+      <li
+        className={`menu-item ${
+          pathName === "/tour-dates/" ? "current-menu-item" : ""
+        }`}
+      >
+        <Link to="/tour-dates/">Tour Dates</Link>
+      </li>
+      <li className="menu-item">
+        <a href="https://goo.gl/NREiMX" target="_blank">
+          Videos
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+const Header = props => {
   const [mobileNavOpen, setMobileNavOpen] = useState(false);
 
   const handleMobileButtonClick = () => {
