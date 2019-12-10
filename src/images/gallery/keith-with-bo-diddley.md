b16fa9aa3e979a99cd9e714aafb8f26cbc4a3487
Adam Arling <adam.arling@northwestern.edu>
2019-12-10 08:07:19 -0600

    Update to gallery photo settings posts

--- src/images/gallery/keith-with-bo-diddley.md
+++
@@ -1,8 +0,0 @@
----
-templateKey: gallery-post
-title: Keith with Bo Diddley
-date: 2019-12-10T02:17:40.035Z
-description: 'Keith hanging out with blues legend, Bo Diddley.'
-featuredimage: /img/keith-bo-diddley.jpg
----
-
---
+++ src/templates/gallery-post.js
@@ -0,0 +1,89 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import { graphql, Link } from "gatsby";
+import Layout from "../components/Layout";
+import Content, { HTMLContent } from "../components/Content";
+
+export const GalleryPostTemplate = ({
+  content,
+  contentComponent,
+  description,
+  title,
+  helmet
+}) => {
+  const PostContent = contentComponent || Content;
+
+  return (
+    <section className="section">
+      {helmet || ""}
+      <div className="container content">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+              {title}
+            </h1>
+            <p>{description}</p>
+            <PostContent content={content} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+GalleryPostTemplate.propTypes = {
+  content: PropTypes.node.isRequired,
+  contentComponent: PropTypes.func,
+  description: PropTypes.string,
+  title: PropTypes.string,
+  helmet: PropTypes.object
+};
+
+const GalleryPost = ({ data }) => {
+  const { markdownRemark: post } = data;
+
+  return (
+    <Layout>
+      <GalleryPostTemplate
+        content={post.html}
+        contentComponent={HTMLContent}
+        description={post.frontmatter.description}
+        helmet={
+          <Helmet titleTemplate="%s | Gallery">
+            <title>{`${post.frontmatter.title}`}</title>
+            <meta
+              name="description"
+              content={`${post.frontmatter.description}`}
+            />
+          </Helmet>
+        }
+        tags={post.frontmatter.tags}
+        title={post.frontmatter.title}
+      />
+    </Layout>
+  );
+};
+
+GalleryPost.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object
+  })
+};
+
+export default GalleryPost;
+
+export const pageQuery = graphql`
+  query GalleryPostByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        description
+        tags
+      }
+    }
+  }
+`;
