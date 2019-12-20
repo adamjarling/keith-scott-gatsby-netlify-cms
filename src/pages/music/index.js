856828c389691568e4556a1e0d4065066f1af7d2
Adam Arling <adam.arling@northwestern.edu>
2019-12-20 10:19:51 -0600

    Update Music page, tour dates to latest and fix favicons

--- src/pages/music/index.js
+++ src/pages/music/index.js
@@ -1,34 +1,24 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import Discography from "../../components/Discography";
 
 const Music = () => {
   return (
     <Layout>
-      <div className="fullwidth-block download" data-bg-color="#191919">
-        <div className="container">
-          <div className="columns">
-            <div className="column is-three-fifths">
-              <h2 className="page-title">Music</h2>
+      <div
+        className="fullwidth-block music inner-content"
+        data-bg-color="#191919"
+      >
+        <div className="container content">
+          <iframe
+            width="100%"
+            height="750"
+            scrolling="no"
+            frameborder="no"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/325361015&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"
+          ></iframe>
 
-              <div className="item">
-                <iframe
-                  width="100%"
-                  height="750"
-                  scrolling="no"
-                  frameborder="no"
-                  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/325361015&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"
-                ></iframe>
-              </div>
-            </div>
-
-            <div className="column is-two-fifths">
-              <aside className="sidebar">
-                <div className="widget">
-                  <h3 className="widget-title">Discography</h3>
-                </div>
-              </aside>
-            </div>
-          </div>
+          <Discography />
         </div>
       </div>
     </Layout>
--- src/tour-dates.js
+++ src/tour-dates.js
@@ -14,96 +14,136 @@ export const monthNames = [
 ];
 
 export const tourDates = [
+  // {
+  //   date: "2019-11-01",
+  //   city: "Sequim",
+  //   state: "WA",
+  //   notes: "",
+  //   venue: "Seven Cedars Casino",
+  //   time: ""
+  // },
+  // {
+  //   date: "2019-11-02",
+  //   city: "Sequim",
+  //   state: "WA",
+  //   notes: "",
+  //   venue: "Hamma Hamma Seafood",
+  //   time: ""
+  // },
+  // {
+  //   date: "2019-11-03",
+  //   city: "Seattle",
+  //   state: "WA",
+  //   notes: "",
+  //   venue: "Washington Blues Society",
+  //   time: ""
+  // },
+  // {
+  //   date: "2019-11-05",
+  //   city: "Seattle",
+  //   state: "WA",
+  //   notes: "",
+  //   venue: "Columbia Tower",
+  //   time: ""
+  // },
+  // {
+  //   date: "2019-11-06",
+  //   city: "Tri Cities",
+  //   state: "WA",
+  //   notes: "",
+  //   venue: "Kimos",
+  //   time: ""
+  // },
+  // {
+  //   date: "2019-11-07",
+  //   city: "Oman",
+  //   state: "WA",
+  //   notes: "",
+  //   venue: "Dawg House",
+  //   time: ""
+  // },
+  // {
+  //   date: "2019-11-08",
+  //   city: "Ellensburg",
+  //   state: "WA",
+  //   notes: "",
+  //   venue: "Reds",
+  //   time: ""
+  // },
+  // {
+  //   date: "2019-11-09",
+  //   city: "Cle Ellum",
+  //   state: "WA",
+  //   notes: "",
+  //   venue: "Mule and Elk Brewery",
+  //   time: ""
+  // },
+  // {
+  //   date: "2019-11-14",
+  //   city: "Chicago",
+  //   state: "IL",
+  //   notes: "",
+  //   venue: "Hyatt",
+  //   time: ""
+  // },
+  // {
+  //   date: "2019-11-15",
+  //   city: "Chicago",
+  //   state: "IL",
+  //   notes: "",
+  //   venue: "The Glenwood",
+  //   time: ""
+  // },
+  // {
+  //   date: "2019-11-16",
+  //   city: "New Buffalo",
+  //   state: "MI",
+  //   notes: "",
+  //   venue: "Dooleys Pub",
+  //   time: ""
+  // },
+  // {
+  //   date: "2019-11-17",
+  //   city: "Chicago",
+  //   state: "IL",
+  //   notes: "",
+  //   venue: "House of Blues",
+  //   time: ""
+  // },
+  // {
+  //   date: "2019-11-22",
+  //   city: "Chicago",
+  //   state: "IL",
+  //   notes: "",
+  //   venue: "Lagunitas Brewery",
+  //   time: ""
+  // },
+  // {
+  //   date: "2019-11-23",
+  //   city: "Saugutuck",
+  //   state: "MI",
+  //   notes: "",
+  //   venue: "Lucys",
+  //   time: ""
+  // },
+  // {
+  //   date: "2019-11-24",
+  //   city: "Bridgeman",
+  //   state: "IL",
+  //   notes: "",
+  //   venue: "Haymarket",
+  //   time: ""
+  // },
+  // {
+  //   date: "2019-11-30",
+  //   city: "Three Oaks",
+  //   state: "MI",
+  //   notes: "",
+  //   venue: "Acorn Theater",
+  //   time: ""
+  // }
   {
-    date: "2019-11-01",
-    city: "Sequim",
-    state: "WA",
-    notes: "",
-    venue: "Seven Cedars Casino",
-    time: ""
-  },
-  {
-    date: "2019-11-02",
-    city: "Sequim",
-    state: "WA",
-    notes: "",
-    venue: "Hamma Hamma Seafood",
-    time: ""
-  },
-  {
-    date: "2019-11-03",
-    city: "Seattle",
-    state: "WA",
-    notes: "",
-    venue: "Washington Blues Society",
-    time: ""
-  },
-  {
-    date: "2019-11-05",
-    city: "Seattle",
-    state: "WA",
-    notes: "",
-    venue: "Columbia Tower",
-    time: ""
-  },
-  {
-    date: "2019-11-06",
-    city: "Tri Cities",
-    state: "WA",
-    notes: "",
-    venue: "Kimos",
-    time: ""
-  },
-  {
-    date: "2019-11-07",
-    city: "Oman",
-    state: "WA",
-    notes: "",
-    venue: "Dawg House",
-    time: ""
-  },
-  {
-    date: "2019-11-08",
-    city: "Ellensburg",
-    state: "WA",
-    notes: "",
-    venue: "Reds",
-    time: ""
-  },
-  {
-    date: "2019-11-09",
-    city: "Cle Ellum",
-    state: "WA",
-    notes: "",
-    venue: "Mule and Elk Brewery",
-    time: ""
-  },
-  {
-    date: "2019-11-14",
-    city: "Chicago",
-    state: "IL",
-    notes: "",
-    venue: "Hyatt",
-    time: ""
-  },
-  {
-    date: "2019-11-15",
-    city: "Chicago",
-    state: "IL",
-    notes: "",
-    venue: "The Glenwood",
-    time: ""
-  },
-  {
-    date: "2019-11-16",
-    city: "New Buffalo",
-    state: "MI",
-    notes: "",
-    venue: "Dooleys Pub",
-    time: ""
-  },
-  {
-    date: "2019-11-17",
+    date: "2019-12-01",
     city: "Chicago",
     state: "IL",
     notes: "",
@@ -111,35 +151,107 @@ export const tourDates = [
     time: ""
   },
   {
-    date: "2019-11-22",
+    date: "2019-12-07",
+    city: "Saulte Sainte Marie",
+    state: "MI",
+    notes: "",
+    venue: "Soo Brewery",
+    time: ""
+  },
+  {
+    date: "2019-12-08",
+    city: "Hart",
+    state: "MI",
+    notes: "",
+    venue: "Big Hart Brewery",
+    time: ""
+  },
+  {
+    date: "2019-12-12",
+    city: "Seattle",
+    state: "WA",
+    notes: "",
+    venue: "Salmon Bay Eagles",
+    time: ""
+  },
+  {
+    date: "2019-12-13",
+    city: "Sequim",
+    state: "WA",
+    notes: "",
+    venue: "Seven Cedars Casino",
+    time: ""
+  },
+  {
+    date: "2019-12-14",
+    city: "Sequim",
+    state: "WA",
+    notes: "",
+    venue: "Seven Cedars Casino",
+    time: ""
+  },
+  {
+    date: "2019-12-14",
+    city: "Hamma",
+    state: "WA",
+    notes: "",
+    venue: "Hamma Seafood",
+    time: ""
+  },
+  {
+    date: "2019-12-15",
+    city: "Port Townsend",
+    state: "WA",
+    notes: "",
+    venue: "Sirens",
+    time: ""
+  },
+  {
+    date: "2019-12-19",
     city: "Chicago",
     state: "IL",
     notes: "",
-    venue: "Lagunitas Brewery",
+    venue: "Mid America",
     time: ""
   },
   {
-    date: "2019-11-23",
-    city: "Saugutuck",
+    date: "2019-12-20",
+    city: "Buelah",
     state: "MI",
     notes: "",
-    venue: "Lucys",
+    venue: "Iron Fish Distillery",
     time: ""
   },
   {
-    date: "2019-11-24",
-    city: "Bridgeman",
-    state: "IL",
-    notes: "",
-    venue: "Haymarket",
-    time: ""
-  },
-  {
-    date: "2019-11-30",
-    city: "Three Oaks",
+    date: "2019-12-21",
+    city: "Ostego",
     state: "MI",
     notes: "",
-    venue: "Acorn Theater",
+    venue: "Liquid Note Brewery",
+    time: ""
+  },
+  {
+    date: "2019-12-22",
+    city: "",
+    state: "MI",
+    notes: "",
+    venue: "2121 Vineyard",
+    time: ""
+  },
+  {
+    date: "2019-12-28",
+    city: "Grand Rapids",
+    state: "MI",
+    notes: "",
+    venue: "Gravel Bottom Brewery",
+    time: ""
+  },
+  {
+    date: "2019-12-31",
+    city: "Frankfort",
+    state: "MI",
+    notes: "",
+    venue: "Storm Cloud Brewery",
     time: ""
   }
 ];
